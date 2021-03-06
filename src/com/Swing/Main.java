f65b5308b38c30b93b658642a6e0cbbc20f35aff
timofeya123 <timofeya123@inbox.ru>
2021-03-06 21:39:35 +0600

    GridBagLayout

--- src/com/Swing/Main.java
+++ src/com/Swing/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         /*
-        // Создание простого BorderLayout
+        // Создание простого ------------------------------------------ BorderLayout ----------------------------------------------
 	    jFrame.add(new JButton("1"), BorderLayout.NORTH); // создание кнопки и его место на экране
         jFrame.add(new JButton("2"), BorderLayout.WEST);   //       NORTH
         jFrame.add(new JButton("3"), BorderLayout.SOUTH);  // WEST CENTER EAST
@@ -18,12 +18,66 @@ public class Main {
         jFrame.add(new JButton("5"), BorderLayout.CENTER);
          */
 
+        /*
+        // Создание простого ------------------------------------------ GridLayout ----------------------------------------------
         int r = 2, c=3; // rows, cols / строка, столбец
         jFrame.add(jPanel);
         jPanel.setLayout(new GridLayout(r,c)); //добавляем на панель наш GridLayout
         for (int i=1; i<=(r*c);i++) {               //      1   2   3
             jPanel.add(new JButton(""+i));      //     4   5   6
         }
+        jFrame.revalidate(); // обновить (перерисовать) элементы окна
+        */
+
+        // Создание простого  ------------------------------------------ GridBagLayout ----------------------------------------------
+        jFrame.add(jPanel);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        jPanel.setLayout(gridBagLayout);
+
+        //               /      Button 1
+        //   TextArea    /--------------------
+        //               / Button 2 / Button 3
+
+        // TextArea
+        GridBagConstraints constraints1 = new GridBagConstraints();
+        constraints1.weightx=0; // сколько будет занимать место, 0 - сам решит
+        constraints1.weighty=0;
+        constraints1.gridx=0; // с какой клетки?
+        constraints1.gridy=0;
+        constraints1.gridheight=2; // сколько займет от целого поля?
+        constraints1.gridwidth=2;
+        jPanel.add(new TextArea(4,20), constraints1);
+
+        // Button 1
+        GridBagConstraints constraints2 = new GridBagConstraints();
+        constraints2.weightx=0;
+        constraints2.weighty=0;
+        constraints2.gridx=2;
+        constraints2.gridy=0;
+        constraints2.gridheight=1;
+        constraints2.gridwidth=2;
+        jPanel.add(new Button("Click 1"), constraints2);
+
+        // Button 2
+        GridBagConstraints constraints3 = new GridBagConstraints();
+        constraints3.weightx=0;
+        constraints3.weighty=0;
+        constraints3.gridx=2;
+        constraints3.gridy=1;
+        constraints3.gridheight=1;
+        constraints3.gridwidth=1;
+        jPanel.add(new Button("Click 2"), constraints3);
+
+        // Button 3
+        GridBagConstraints constraints4 = new GridBagConstraints();
+        constraints4.weightx=0;
+        constraints4.weighty=0;
+        constraints4.gridx=3;
+        constraints4.gridy=1;
+        constraints4.gridheight=1;
+        constraints4.gridwidth=1;
+        jPanel.add(new Button("Click 3"), constraints4);
+
         jFrame.revalidate(); // обновить (перерисовать) элементы окна
     }
 
